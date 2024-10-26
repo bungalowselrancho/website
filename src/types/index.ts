c4c3595319f2f8fe3032a09faf4f0dfe8bcbc9c7
Tobías Facello <l>
2024-10-26 19:05:52 -0300

    Updates types declarations

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,22 +1,24 @@
-//* Next Types
+//! Next Types
 import { StaticImageData } from 'next/image';
 
-//* SVG Component
+//* SVG
 export type SVGProps = {
+	className?: string;
 	fill?: string;
 	width: number;
 	height: number;
 };
 
-//* Title Component
+//* Title
 export type TitleProps = {
 	variant?: string;
 	font?: string;
 	color?: string;
+	partOf?: string;
 	children: React.ReactNode;
 };
 
-//* Text Component
+//* Text
 export type TextProps = {
 	variant?: string;
 	font?: string;
@@ -25,7 +27,7 @@ export type TextProps = {
 	children: React.ReactNode;
 };
 
-//* Button Component
+//* Button
 export type ButtonProps = {
 	variant: string;
 	size: string;
@@ -33,7 +35,12 @@ export type ButtonProps = {
 	children: React.ReactNode;
 };
 
-//* Card Component
+//* Avatar
+export type AvatarProps = {
+	src: StaticImageData;
+};
+
+//* Card
 export type CardProps = {
 	tag: string;
 	image: StaticImageData;
@@ -44,7 +51,41 @@ export type CardProps = {
 	children?: React.ReactNode;
 };
 
-//* Tag Component
+//* Details Card
+export type DetailsCardProps = {
+	title: string;
+	details: string;
+	children?: React.ReactNode;
+};
+
+//* Testimonial Card
+export type TestimonialCardProps = {
+	rate: number[];
+	comment: string;
+	avatar: StaticImageData;
+	author: string;
+	date: string;
+	children?: React.ReactNode;
+};
+
+//* Tag
 export type TagProps = {
 	children?: React.ReactNode;
+};
+
+//* Carousel
+export type CarouselItemType = {
+	title: string;
+	children: React.ReactNode;
+};
+
+export type CarouselProps = {
+	items: CarouselItemType[];
+};
+
+//* Accordion
+
+export type AccordionProps = {
+	question: string;
+	answer: React.ReactNode | string;
 };
