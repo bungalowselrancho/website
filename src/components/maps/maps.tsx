bada83bd612ea87d7f932729e8ee8e48e8769c6e
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2025-09-01 16:24:04 -0300

    Fixes Maps component (removes Google Maps API usage and embeds a generic map)

--- src/components/maps/maps.tsx
+++ src/components/maps/maps.tsx
@@ -1,35 +1,19 @@
 'use client';
 
-//* Google Maps
-import { APIProvider, Map, AdvancedMarker } from '@vis.gl/react-google-maps';
-
 //* Styles
 import styles from './styles.module.css';
 
 export default function Maps() {
 	return (
 		<>
-			<APIProvider apiKey={'AIzaSyAvsNSqJu6fPHUXyivHkZEK7E4mROBwL7E'}>
-				<Map
-					mapId={'311ac32c3a732a0a'}
-					className={styles.map}
-					defaultCenter={{
-						lat: -32.21541213989258,
-						lng: -58.21834182739258,
-					}}
-					defaultZoom={16}
-					gestureHandling={'greedy'}
-					disableDefaultUI={true}
-					colorScheme={'FOLLOW_SYSTEM'}
-				>
-					<AdvancedMarker
-						position={{
-							lat: -32.21541213989258,
-							lng: -58.21834182739258,
-						}}
-					/>
-				</Map>
-			</APIProvider>
+			<iframe
+				width="600"
+				height="450"
+				loading="lazy"
+				className={styles.map}
+				allowFullScreen
+				src="https://www.google.com/maps?q=-32.21541213989258,-58.21834182739258&z=15&output=embed">
+			</iframe>
 		</>
 	);
 }
